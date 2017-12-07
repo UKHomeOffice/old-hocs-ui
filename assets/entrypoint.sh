f05e6af2f76efa038ad5731533e0f6b0aa183486
nickmcmahonHOD <nick.mcmahon@digital.homeoffice.gov.uk>
2017-12-07 23:25:13 +0000

    add assetic dump

--- assets/entrypoint.sh
+++ assets/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 composer install
+bin/console assets:install --symlink
+bin/console assetic:dump
 
 sed -i  's/${APP_ENV}/'"$APP_ENV"'/' /etc/nginx/sites-enabled/symfony
 sed -i  's/${APP_PHP}/'"$APP_PHP"'/' /etc/nginx/sites-enabled/symfony
