7658843786d8444ffb8a774596185ae87a65f5be
nickmcmahonHOD <nick.mcmahon@digital.homeoffice.gov.uk>
2017-12-08 17:32:36 +0000

    Reset https options

--- assets/entrypoint.sh
+++ assets/entrypoint.sh
@@ -3,7 +3,6 @@
 sed -i  's/${APP_ENV}/'"$APP_ENV"'/' /etc/nginx/sites-enabled/symfony
 sed -i  's/${APP_PHP}/'"$APP_PHP"'/' /etc/nginx/sites-enabled/symfony
 
-curl https://curl.haxx.se/ca/cacert.pem > /etc/ssl/certs/cacert.pem
 echo >> /etc/ssl/certs/cacert.pem
 cat /data/hocs-ui-ca.pem >> /etc/ssl/certs/cacert.pem
 
