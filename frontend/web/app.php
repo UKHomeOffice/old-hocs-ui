9bd775dedb4c0ad1f6a1fac2c924845b09aa9fc2
nickmcmahonHOD <nick.mcmahon@digital.homeoffice.gov.uk>
2018-01-05 12:34:52 +0000

    Problem with Dev routing.

--- frontend/web/app.php
+++ frontend/web/app.php
@@ -5,16 +5,7 @@ use Symfony\Component\HttpFoundation\Request;
 
 $loader = require_once __DIR__.'/../var/bootstrap.php.cache';
 
-// Use APC for autoloading to improve performance.
-// Change 'sf2' to a unique prefix in order to prevent cache key conflicts
-// with other applications also using APC.
-$apcLoader = new ApcClassLoader('hocssf', $loader);
-$loader->unregister();
-$apcLoader->register(true);
-
-
 require_once __DIR__.'/../app/AppKernel.php';
-require_once __DIR__.'/../app/AppCache.php';
 
 $appEnv = "prod";
 
@@ -35,10 +26,7 @@ if ($appEnv == "dc") {
 
 $kernel = new AppKernel($appEnv, $debugMode);
 $kernel->loadClassCache();
-$kernel = new AppCache($kernel);
 
-// When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
-Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
